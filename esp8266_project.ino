df4184a3047557dfeef27d6080f5d9ad32af35eb
hydroshy <phanloi1403@gmail.com>
2022-11-26 11:30:04 +0700

    add ino file of project

---
+++ esp8266_project.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+#include <ESP8266WebServer.h>
+//HMTL webpage contents
+#include "index.h"
+//SSID and Password of Wifi router
+const char* ssid = "";
+const char* password = ""; 
+
+ESP8266WebServer server(80); //Port of server
+
+void setup() {
+  Serial.begin(9600);
+  
+  //Connect to your WiFi router
+  WiFi.begin(ssid, password);     
+
+  //Wait for pass the security
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //Show up information when connection successful
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP())
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
